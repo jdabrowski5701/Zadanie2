84795aa1d7591c5ee4d241d628c0c4cd65668cdc
Jakub Dąbrowski <jakubdabrowski5701@gmail.com>
2023-10-09 13:13:53 +0200

    Logs, onSaveInstanceState

--- app/src/main/java/com/example/zadanie2/MainActivity.java
+++ app/src/main/java/com/example/zadanie2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.zadanie2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,6 +11,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String KEY_CURRENT_INDEX = "currentIndex";
     private Button trueButton;
     private Button falseButton;
     private Button nextButton;
@@ -25,6 +27,13 @@ public class MainActivity extends AppCompatActivity {
 
     private int currentIndex = 0;
     private int score = 0;
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        Log.d("MainActivity", "onSaveInstanceState() wywolane");
+        outState.putInt(KEY_CURRENT_INDEX, currentIndex);
+    }
 
     private void checkAnswerCorrectness(boolean userAnswer){
         boolean correctAnswer = questions[currentIndex].isTrueAnswer();
@@ -47,10 +56,17 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, resultMessage, Toast.LENGTH_SHORT).show();
     }
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("MainActivity", "onCreate() wywolane");
+
         setContentView(R.layout.activity_main);
+
+        if(savedInstanceState != null){
+            currentIndex = savedInstanceState.getInt(KEY_CURRENT_INDEX);
+        }
 
         trueButton = findViewById(R.id.true_button);
         falseButton = findViewById(R.id.false_button);
@@ -59,31 +75,49 @@ public class MainActivity extends AppCompatActivity {
 
         setNextQuestion();
 
-        trueButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                checkAnswerCorrectness(true);
+        trueButton.setOnClickListener(v -> checkAnswerCorrectness(true));
+
+        falseButton.setOnClickListener(v -> checkAnswerCorrectness(false));
+
+        nextButton.setOnClickListener(v -> {
+            currentIndex = (currentIndex + 1) % questions.length;
+            if (currentIndex == 0) {
+                quizSummary();
+                score = 0;
             }
+            setNextQuestion();
         });
 
-        falseButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                checkAnswerCorrectness(false);
-            }
-        });
 
-        nextButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                currentIndex = (currentIndex + 1) % questions.length;
-                if (currentIndex == 0) {
-                    quizSummary();
-                    score = 0;
-                }
-                setNextQuestion();
-            }
-        });
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("MainActivity", "onStart() wywolane");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("MainActivity", "onResume() wywolane");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("MainActivity", "onStop() wywolane");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("MainActivity", "onPause() wywolane");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("MainActivity", "onDestroy() wywolane");
     }
 }
